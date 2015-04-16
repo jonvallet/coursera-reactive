2afc0f4016f76755a5fde2cdd216a4a76491f512
Jon Vallet <jon.vallet@proxama.com>
2015-04-16 13:37:39 +0100

    Implemented a generator to apply properties to all the implementations

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,15 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  property("min1") = forAll { a: Int =>
-    val h = insert(a, empty)
-    findMin(h) == a
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h))==m
   }
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    n <- arbitrary[Int]
+    heap <- oneOf(const(insert(n, empty)), genHeap)
+  } yield heap
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
